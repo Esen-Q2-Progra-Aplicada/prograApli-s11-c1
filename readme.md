427dd8a19efda78be30df1b3bb149b66ccbcc89f
baylagas <balbino_a@hotmail.com>
2021-07-20 18:24:42 -0600

    init files

---
+++ readme.md
@@ -0,0 +1,7 @@
+# flask ci cd example alpha
+
+## crear el repo con master branch
+se debe crear un repo con un master branch
+
+## crear una branch development
+crear una branch development que luego probaremos
---
+++ tests/test_webapp.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from routes.main_routes import MainRoutes
+import requests
+
+
+def test_webapp():
+    assert True is True
+
+
+def test_webapp_site_home():
+    app = Flask(__name__, template_folder="../templates")
+    MainRoutes.configure_routes(app)
+    client = app.test_client()
+
+    response = client.get("/")
+    assert response.status_code == 200
